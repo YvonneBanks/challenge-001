306c1cda401016a3f27a72548a2e446d3173a6f8
YvonneBanks <nabsyvonne2018@gmail.com>
2019-11-27 16:02:13 +0000

    added my javascript work

---
+++ string.js
@@ -0,0 +1,27 @@
+const vowels = ['a','e','i','o','u'];
+var vowelsString =[];
+var duplicates = 0;
+function vowel_count(str1){
+    for (const letter of str1) {
+    for (const vowel of vowels) {
+      if (vowel===letter) {
+        if(vowelsString.includes(letter)){
+         ++duplicates
+        }else{
+          vowelsString+= letter;
+        }
+        
+      }
+    }
+    
+        
+      }
+      return ( `('${vowelsString}', ${duplicates})`); 
+    }
+  
+
+    
+
+    
+
+  console.log(vowel_count("aroplane"));
---
+++ variables.js
@@ -0,0 +1,8 @@
+var age = 40;
+if (age <= 18){
+  console.log("you are minor");
+}else if (age <=36){
+    console.log("you are a youth")};
+     if (age > 36){
+     console.log("you are an elder")
+ }
